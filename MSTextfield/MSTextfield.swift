c3543966e08b14923ee930f9ec2721b7b80fb655
Marco Sportillo <msportillo@gmail.com>
2016-10-08 18:39:25 +0200

    Adding background icon

--- MSTextfield/MSTextfield.swift
+++ MSTextfield/MSTextfield.swift
@@ -11,30 +11,44 @@ import UIKit
 
 @IBDesignable
 class MSTextfield: NibDesignable, UITextFieldDelegate {
-    
+    //IBOutlet
     @IBOutlet weak var icon: UIImageView!
     @IBOutlet weak var border: UIImageView!
     @IBOutlet weak var textfield: UITextField!
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var error: UILabel!
-
-    @IBInspectable var iconBorder: CGFloat = 1.0
-    @IBInspectable var borderColor: UIColor = UIColor.red
+    
+    //Inspectable for icon
+    @IBInspectable var backgroundIcon: UIColor = UIColor.clear
     @IBInspectable var iconImage:  UIImage = UIImage()
+
+    //Inspetacable for border
+    @IBInspectable var borderColor: UIColor = UIColor.red
+    
+    //Inspectable for textfield
     @IBInspectable var placeholder: String = ""
+    @IBInspectable var placeholderColor: UIColor = UIColor.lightGray
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        
         prepareForInterfaceBuilder()
-        self.border.backgroundColor = borderColor
+        
         self.icon.image = iconImage
         self.textfield.placeholder = placeholder
+        
+        self.icon.backgroundColor = backgroundIcon
+        self.textfield.attributedPlaceholder = NSAttributedString(string:placeholder,
+                                                               attributes:[NSForegroundColorAttributeName:placeholderColor])
+        self.border.backgroundColor = borderColor
+        
     }
     
     override public func prepareForInterfaceBuilder() {
         self.border.backgroundColor = borderColor
         self.icon.image = iconImage
         self.textfield.placeholder = placeholder
+        self.icon.backgroundColor = backgroundIcon
+        self.textfield.attributedPlaceholder = NSAttributedString(string:placeholder,
+                                                                  attributes:[NSForegroundColorAttributeName:placeholderColor])
     }
 }
